f46d1e5a39c928745d432790890dc102a549d581
Darmawan636 <ajidarmawan1@gmail.com>
2021-04-01 00:06:35 +0900

    Tugas Portofolio

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,6 +6,8 @@
     <div class="row">
         <div class="col-12">
             <h2 class="mt-3 alert alert-warning">Selamat Datang </h2>
+            <h3>Nama saya I Nyoman Darmawan Setiaji</h3>
+            <h3>Saya tinggal di Pajang Baru</h3>
         </div>
     </div>
 @endsection
--- resources/views/tentang.blade.php
+++ resources/views/tentang.blade.php
@@ -4,4 +4,7 @@
 
 @section('isi')
     <h2 class="mt-3">Tentang Saya</h2>
+    <h3>Saya adalah masahasiswa Universitas Bumigora</h3>
+    <h3>Program Studi Ilmu Komputer</h3>
+    <h3>Jenjang Strata 1</h3>
 @endsection
